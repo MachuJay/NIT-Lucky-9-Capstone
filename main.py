8d2509f00590299b09e459a39ad2f502e1373e74
MachuJay <125789418+MachuJay@users.noreply.github.com>
2024-11-20 03:43:38 +0800

    Stable Initial Build

--- main.py
+++ main.py
@@ -1,15 +1,49 @@
-import pandas as pd
+# Import packages
+from tkinter import *
 
-# Set Grocery Items Inventory Excel file's location then read
-file_path = 'Resources/Inventory.xlsx'
-df = pd.read_excel(file_path)
-# df = pd.read_excel(file_path, sheet_name='Sheet1') *** [If multiple sheets]
+# Define Classes
+class App(Tk):
+    def __init__(self):
+        super().__init__()
 
-# Display Excel file contents
-print(df)
+        # Initialize window properties
+        self.title("Puregro Virtual Shopping Cart")
+        self.iconbitmap('resources/puregro.ico')
+        self.resizable(False, False)
 
-# Test Printing
-print("Lucky 9")
+        # Center program window on screen
+        self.window_height = 450
+        self.window_width = 700
+        self.screen_width = self.winfo_screenwidth()
+        self.screen_height = self.winfo_screenheight()
+        self.x_coordinate = int((self.screen_width/2) - (self.window_width/2))
+        self.y_coordinate = int((self.screen_height/2) - (self.window_height/2))
+        self.geometry("{}x{}+{}+{}".format(self.window_width, self.window_height, self.x_coordinate, self.y_coordinate))
 
-# User input to end program
-sayonara = input("Enter any key to exit...")
+        # Variables
+        self.togglestatus = True
+
+        # Brand Intro Banner
+        self.image_banner = PhotoImage(file="resources/banner_puregro.png")
+        self.my_labelimage = Label(self, image=self.image_banner)
+        self.my_labelimage.pack(pady=20)
+        
+        # Label Box
+        self.my_label = Label(self, text="Hello Lucky 9!", font=("Helvetica", 42))
+        self.my_label.pack(pady=20)
+
+        # Toggle Button
+        self.my_button = Button(self, text = "Toggle", command=self.change)
+        self.my_button.pack(pady=20)
+
+    def change(self):
+        if self.togglestatus == True:
+            self.my_label.config(text="GUI App Initialized.")
+            self.togglestatus = False
+        else:
+            self.my_label.config(text="Hello Lucky 9!")
+            self.togglestatus = True
+
+# Define and Instantiate Shopping Cart App 
+app = App()
+app.mainloop()
---
+++ misc.py
@@ -0,0 +1,83 @@
+''' --------------------------------------------------------------------------------------------------- '''
+# User input to end program
+sayonara = input("Enter any key to exit...")
+
+''' --------------------------------------------------------------------------------------------------- '''
+# ---------------------------------------------------------------------------------------------------
+'''Nuitka Building
+nuitka --standalone --onefile --output-dir=build --windows-icon-from-ico=.\resources\puregro.ico --include-data-dir=.\resources=resources --enable-plugin=tk-inter main.py
+
+--standalone: Creates a standalone application, including all necessary dependencies.
+--onefile: Packages everything into a single executable file.
+--output-dir=dist: Specifies the directory where the output file will be stored.
+
+To include the resources/ directory:
+nuitka --standalone --onefile --include-data-dir=resources=resources app.py
+To include individual files:
+nuitka --standalone --onefile --include-data-file=resources/config.json=resources/config.json 
+       --include-data-file=resources/logo.png=resources/logo.png app.py
+
+Adding an icon:
+nuitka --standalone --onefile --windows-icon-from-ico=icon.ico app.py
+Optimizing for performance:
+nuitka --standalone --onefile --lto --follow-imports app.py
+'''
+# ---------------------------------------------------------------------------------------------------
+''' Python Debugger
+You can launch a Python program through pdb via python -m pdb myscript.py.
+
+There are a few commands you can then issue, which are documented on the pdb page.
+
+Some useful ones to remember are:
+
+b: set a breakpoint
+c: continue debugging until you hit a breakpoint
+s: step through the code
+n: to go to next line of code
+l: list source code for the current file (default: 11 lines including the line being executed)
+u: navigate up a stack frame
+d: navigate down a stack frame
+p: to print the value of an expression in the current context
+'''
+# ---------------------------------------------------------------------------------------------------
+''' Excel File Reader
+import pandas as pd
+
+# Set Grocery Items Inventory Excel file's location then read
+file_path = 'Resources/Inventory.xlsx'
+df = pd.read_excel(file_path)
+# df = pd.read_excel(file_path, sheet_name='Sheet1') *** [If multiple sheets]
+
+# Display Excel file contents
+print(df)
+'''
+# ---------------------------------------------------------------------------------------------------
+''' Main Class Code Zero
+class App(Tk):
+    def __init__(self):
+        super().__init__()
+
+        # Initialize window properties
+        self.title("Puregro Shopping Cart")
+        self.iconbitmap('resources/puregro.ico')
+        self.geometry('700x450')
+
+        # Variables
+        self.status = True
+
+        # Widgets
+        self.my_label = Label(self, text="Hello World!", font=("Helvetica", 42))
+        self.my_label.pack(pady=20)
+
+        self.my_button = Button(self, text = "Change Text", command=self.change)
+        self.my_button.pack(pady=20)
+
+    def change(self):
+        if self.status == True:
+            self.my_label.config(text="Goodbye World!")
+            self.status = False
+        else:
+            self.my_label.config(text="Hello World!")
+            self.status = True
+'''
+# ---------------------------------------------------------------------------------------------------
