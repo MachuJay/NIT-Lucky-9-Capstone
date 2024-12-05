1b18d3a5a634ae85695411e90466f57e14d5a8f2
MachuJay <125789418+MachuJay@users.noreply.github.com>
2024-12-05 10:03:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class ShoppingCartSystem(Tk):
     # Initialize Order Variables
     orderlist_counters = {}
     orderlist_images = []
+    adminlist_prices = {}
     total_quantity = None
     # Initialize Database Rows
     rows_items = None
@@ -575,12 +576,13 @@ class frame_admin_home(Frame):
         super().__init__(parent)
         self.parent = parent
 
+        # Inventory List View
         self.canvastime(parent)
 
-        '''for x in range(50):
-            Label(self.frame, text="placeholder", font=("Segoe UI", 10, "bold"), fg="white", bg="black").grid(row=x, column=0, pady=10, padx=0)'''
-    
-    # test
+        # Other admin tasks
+        #placeholder
+
+    # Inventory List View
     def canvastime(self, parent):
         # Initialize Canvas
         self.canvas = Canvas(parent)
@@ -613,7 +615,6 @@ class frame_admin_home(Frame):
         Label(self.frame, text="").grid(row=0, column=3, pady=10, padx=10)
         Label(self.frame, text="CATEGORY", font=("Segoe UI", 10, "bold"), fg="white", bg="black").grid(row=0, column=4, pady=10, padx=0)
         # Display retrieved "inventory" table rows
-        self.item_placeholder = PhotoImage(file=parent.resource_path("resources/placeholder.png"))
         self.rowcounter = 0
         for self.row in parent.rows_items:
             self.rowcounter += 1
@@ -622,22 +623,24 @@ class frame_admin_home(Frame):
             # quantity
             Label(self.frame, text=self.row[2], font=("Tahoma", 16, "")).grid(row=self.rowcounter, column=1, pady=0, padx=0)
             # price
-            Label(self.frame, text=f"Php {self.row[3]}", font=("Tahoma", 16, "")).grid(row=self.rowcounter, column=2, pady=0, padx=0)
+            self.label_price = Label(self.frame, text=f"Php {self.row[3]}", font=("Tahoma", 16, ""))
+            self.label_price.grid(row=self.rowcounter, column=2, pady=0, padx=0)
+            parent.adminlist_prices[self.rowcounter-1] = self.label_price
             # adjust price buttons
             self.fg = ""
             self.bg = ""
             if self.rowcounter % 2 == 0:
-                self.fg = "white"
-                self.bg = "black"
-            else:
                 self.fg = "black"
                 self.bg = "white"
+            else:
+                self.fg = "white"
+                self.bg = "black"
             Button(self.frame, text="Adjust Price", font=("Tahoma", 12, "bold"), fg=self.fg, bg=self.bg, command=lambda id=self.row[0]: self.adjustprice(id)).grid(row=self.rowcounter, column=3, pady=0, padx=0)
             # category
             Label(self.frame, text=self.row[4], font=("Tahoma", 16, "")).grid(row=self.rowcounter, column=4, pady=0, padx=23)
     # Mouse Scroll Wheel event
     def on_mousewheel(self, event):
-            self.canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
+        self.canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
     # Activate/Deactivate mousewheel scrolling when mouse cursor is over/not over the respective widget
     def set_mousewheel(self, widget, command):
         widget.bind("<Enter>", lambda _: widget.bind_all('<MouseWheel>', command))
@@ -648,19 +651,67 @@ class frame_admin_home(Frame):
         self.cart.focus_set()
         self.cart.grab_set()
         # Set Cart Summary popup window's properties and center position to screen
-        self.cart.title("Cart Summary and Checkout")
+        self.cart.title("Pricing")
         self.cart.iconbitmap(self.parent.path_icon)
         self.cart.resizable(False, False)
-        self.window_height = 500
-        self.window_width = 470
+        self.window_height = 230
+        self.window_width = 380
         self.screen_width = self.winfo_screenwidth()
         self.screen_height = self.winfo_screenheight()
         self.x_coordinate = int((self.screen_width/2) - (self.window_width/2))
         self.y_coordinate = int((self.screen_height/2) - (self.window_height/2))
         self.cart.geometry("{}x{}+{}+{}".format(self.window_width, self.window_height, self.x_coordinate, self.y_coordinate))
         # User input to change item price
-        print(id)
-        Label(self.frame, text="NAME", font=("Segoe UI", 10, "bold"), fg="white", bg="black").grid(row=0, column=0, pady=10, padx=0)
+        self.row_adjust = None
+        for self.row_item in self.parent.rows_items:
+            if (self.row_item[0] == id):
+                self.row_adjust = self.row_item
+        Label(self.cart, text="-----------ADJUST ITEM PRICE-----------", font=("Segoe UI", 16, "bold"), fg="white", bg="black").grid(row=0, column=0, pady=0, padx=0, columnspan=2)
+        Label(self.cart, text="NAME", font=("Segoe UI", 16, "bold"), fg="white", bg="#A21F6A").grid(row=1, column=0, pady=3, padx=0, sticky=W)
+        Label(self.cart, text=self.row_adjust[1], font=("Segoe UI", 18, "bold"), fg="black").grid(row=1, column=1, pady=3, padx=0, sticky=W)
+        Label(self.cart, text="PRICE", font=("Segoe UI", 16, "bold"), fg="white", bg="#A21F6A").grid(row=2, column=0, pady=3, padx=0, sticky=W)
+        Label(self.cart, text=f"Php {self.row_adjust[3]}", font=("Segoe UI", 16, "bold"), fg="black").grid(row=2, column=1, pady=3, padx=0, sticky=W)
+        Label(self.cart, text="New Price:", font=("Segoe UI", 16, "bold"), fg="black").grid(row=3, column=0, pady=3, padx=0, sticky=W)
+        self.input_price = Entry(self.cart, font=("Segoe UI", 16, "bold"), fg="black", width=18)
+        self.input_price.grid(row=3, column=1, pady=3, padx=0, sticky=W)
+        # confirm button
+        Button(self.cart, text="CONFIRM", font=("Tahoma", 16, ""), fg="#A21F6A", bg="black", command=lambda id=id, name=self.row_adjust[1], entryfield=self.input_price: self.price_update(id, name, entryfield)).grid(row=4, column=0, pady=8, padx=0, columnspan=2)
+    def price_update(self, id, name, entryfield):
+        self.price = entryfield.get()
+        # Check if input is valid
+        try:
+            # Convert string to float
+            self.price = float(self.price)
+            # Convert float to string with 2 decimal places
+            self.price = f"{self.price:.2f}"
+            # Convert string to float then round up float value to 2 decimal places
+            self.price = round(float(self.price), 2)
+            # Database Access: Update inventory item's price column
+            self.parent.db_connect("announce")
+            self.cursor = self.parent.connection.cursor()
+            self.cursor.execute(f"UPDATE inventory SET price = {self.price} WHERE id_item = {id}")
+            print(f"    Updating price of \'{name}\' from \'{self.row_adjust[3]}\' to \'{self.price}\'...")
+            self.parent.connection.commit()
+            print(f"    Succesfully updated price to \'{self.price}\'")
+            # Query updated price's database value
+            self.cursor = self.parent.connection.cursor()
+            self.cursor.execute(f"SELECT * FROM inventory WHERE id_item = {id}")
+            self.row_item = self.cursor.fetchone()
+            self.parent.db_disconnect("announce")
+            # Update Price label's value
+            self.rowcounter = 0
+            # Traverse inventory items list
+            for self.row in self.parent.rows_items:
+                # If current row is equal to recently modified item's price row, update label's text value
+                if (self.row[0] == id):
+                    self.parent.adminlist_prices[self.rowcounter].config(text=f"Php {self.row_item[3]}")
+                self.rowcounter += 1
+            # Exit popup window
+            self.cart.destroy()
+        except Error as e:
+            print("Error while connecting to MySQL:", e)
+        except:
+            print("    Invalid Input.")
         
 #-----------------------------------------------------------------------------------------------------------------
 #--- RUNTIME INITIATE --------------------------------------------------------------------------------------------
